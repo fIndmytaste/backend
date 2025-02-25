c1f52e7a4fb34a93c7d6acbc740b5a9edf8edcbc
olakayCoder1 <programmerolakay@gmail.com>
2025-02-26 00:42:17 +0100

    `Update settings.py and requirements.txt for static file configuration`

--- findmytaste/settings.py
+++ findmytaste/settings.py
@@ -122,7 +122,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Location to store static files (in development mode, static files will be stored in this folder)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# List of directories to look for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 MEDIA_URL = '/media/'
