6a62d9e8d0748728b152ced65a744216cdfa27c6
olakayCoder1 <programmerolakay@gmail.com>
2025-04-20 16:42:32 +0100

    `Remove vendor prefix from URL patterns`

--- vendor/urls.py
+++ vendor/urls.py
@@ -9,9 +9,9 @@ from .views import (
 
 urlpatterns = [
     # Vendor Endpoints
-    path('vendor/register-business/', VendorRegisterBusinessView.as_view(), name='vendor_register_business'),
-    path('vendor/category/', VendorCategoryView.as_view(), name='add_vendor_category'),
-    path('vendor/product/', ProductsListCreateView.as_view(), name='list_add_product'),
-    path('vendor/product/<uuid:product_id>/', ProductGetUpdateDeleteView.as_view(), name='edit_delete_product'),
-    path('vendor/orders', VendorOrderListView.as_view(), name='order_list'),
+    path('register-business/', VendorRegisterBusinessView.as_view(), name='vendor_register_business'),
+    path('category/', VendorCategoryView.as_view(), name='add_vendor_category'),
+    path('product/', ProductsListCreateView.as_view(), name='list_add_product'),
+    path('product/<uuid:product_id>/', ProductGetUpdateDeleteView.as_view(), name='edit_delete_product'),
+    path('orders', VendorOrderListView.as_view(), name='order_list'),
 ]
