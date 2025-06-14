7df152120f43aecb1e8aae6e7d2d4edb6d19ca91
olakayCoder1 <programmerolakay@gmail.com>
2025-06-14 10:25:31 +0100

    "Added user field to AdminWalletTransactionSerializer with SerializerMethodField and implemented get_user method to return user details."

--- admin_manager/serializers/transactions.py
+++ admin_manager/serializers/transactions.py
@@ -4,6 +4,17 @@ from wallet.models import WalletTransaction
 
 
 class AdminWalletTransactionSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
     class Meta:
         model = WalletTransaction
         fields = '__all__'
+
+
+    def get_user(self,obj:WalletTransaction):
+        if obj.wallet:
+            return {
+                'id':obj.wallet.user.id,
+                'full_name':obj.wallet.user.full_name,
+            }
+        else:
+            return None
