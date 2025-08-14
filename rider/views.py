bfa5a2c1aa62548374d47b74102edc0b22e1d77e
olakayCoder1 <programmerolakay@gmail.com>
2025-08-14 17:25:09 +0100

    `Removed three print statements from ConfirmOrderPaymentAPIView`

--- rider/views.py
+++ rider/views.py
@@ -253,9 +253,6 @@ class ConfirmOrderPaymentAPIView(generics.GenericAPIView):
             if response['data'].get('status') == 'success':
                 #  confirm the amount paid
                 amount_paid = (response['data']['amount']) / 100
-                print(amount_paid)
-                print(amount_paid)
-                print(amount_paid)
                 if float(amount_paid) != float(trx_extist.amount):
                     return bad_request_response(
                         message="Transaction not doest exist. Amount paid does not match"
